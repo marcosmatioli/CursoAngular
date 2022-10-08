2edef5ccd2602a60e3ea05bc05f3115941fc7182
marcosmatioli <100784085+marcosmatioli@users.noreply.github.com>
2022-10-07 22:56:35 -0300

    ngOnChanges, ngDoCheck, ngAfters... e ngDestroy

--- relembrando-angular/src/app/app.component.ts
+++ relembrando-angular/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentChecked,AfterContentInit,AfterViewInit,AfterViewChecked, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: `
+  <app-tittle tittle="Bem vindo"></app-tittle>
+  {{valor}}
+  <button (click)="adicionar()">Adicionar</button>
+  <router-outlet></router-outlet>`
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit, OnChanges, DoCheck, AfterContentChecked,AfterContentInit,AfterViewInit,AfterViewChecked {
+
+  public valor: number = 5;
+
+
+  constructor(){}
+
+  public adicionar(){
+    return this.valor += 1;
+  }
+
+  ngOnInit(): void { // quando inicia o componente ele faz isso aqui ou seja na inicialização do componente
+    console.log("Componente ONINIT")
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Componente ngONChanges")
+    
+  }
+  ngDoCheck(): void {
+    console.log("Componente doCheck")
+    
+  }
+  ngAfterContentChecked(): void {
+    console.log("Componente AfterContentCheckec")
+    
+  }
+  ngAfterContentInit(): void {
+    console.log("Componente ngAfterContentInit")
+    
+  }
+  ngAfterViewChecked(): void {
+    console.log("Componente ngAfterViewChecked")
+    
+  }
+  ngAfterViewInit(): void {
+    console.log("Componente ngAfterViewInit")
+    
+  }
   
 }
--- relembrando-angular/src/app/tittle/tittle.component.ts
+++ relembrando-angular/src/app/tittle/tittle.component.ts
@@ -1,17 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input, DoCheck, AfterContentChecked,AfterContentInit,AfterViewInit,AfterViewChecked  } from '@angular/core';
 
 @Component({
   selector: 'app-tittle',
   templateUrl: './tittle.component.html',
   styleUrls: ['./tittle.component.scss']
 })
-export class TittleComponent implements OnInit {
+export class TittleComponent implements OnInit, OnChanges, DoCheck, AfterContentChecked,AfterContentInit,AfterViewInit,AfterViewChecked  {
 
-  public tittle:string = "Bem Vindo!";
+  @Input() public tittle: string = 'Bem Vindo';
+  
   constructor() { }
 
+
   ngOnInit(): void { // quando inicia o componente ele faz isso aqui ou seja na inicialização do componente
+    console.log("Componente ONINIT")
     
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Componente ngONChanges")
+    
+  }
+  ngDoCheck(): void {
+    console.log("Componente doCheck")
+    
+  }
+  ngAfterContentChecked(): void {
+    console.log("Componente AfterContentCheckec")
+    
+  }
+  ngAfterContentInit(): void {
+    console.log("Componente ngAfterContentInit")
+    
+  }
+  ngAfterViewChecked(): void {
+    console.log("Componente ngAfterViewChecked")
+    
+  }
+  ngAfterViewInit(): void {
+    console.log("Componente ngAfterViewInit")
+    
+  }
 }

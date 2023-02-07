73b853104cf7db36bce90489534543cba6e7d1a8
marcosmatioli <100784085+marcosmatioli@users.noreply.github.com>
2023-02-06 21:53:54 -0300

    Selected, Erros(Dirty,Valid,Invalid,Touched) and submit form

--- relembrando-angular/src/app/forms/forms.component.ts
+++ relembrando-angular/src/app/forms/forms.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  public listComidas : Array<{comida: string, preco : string}> = [
+    {comida : "X-Salada", preco: "R$10,00"},
+    {comida : "X-Egg", preco: "R$5,00"},
+    {comida : "X-Bacon", preco: "R$20,00"}
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public submitForm(form: any){
+    if(form.valid){
+      console.log(form.value)
+    }
+  }
 }
+
+  
